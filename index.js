5849a653a34650688bfeb2ec1d68e7c87d1ce55d
herosql <15074798673@163.com>
2024-09-15 15:04:34 +0800

    feat:finish API

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// target API
+app.get('/api/whoami',function (req,res){
+  res.json({ipaddress:req.headers['x-forwarded-for'],
+    language:req.get('Accept-Language'),
+    software:req.get('user-agent')});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
